8ae2768cf1151aaf0892fd11c6ad1c8f853b0508
Mamadu-Gaye <mamadugay00@gmail.com>
2024-03-28 07:11:35 +0000

    Checkpoint-ReactProps

---
+++ src/player.js
@@ -0,0 +1,21 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentine",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portugais",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    // Ajoutez d'autres joueurs selon votre choix
+  ];
+  
+  export default players;
